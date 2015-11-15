1672c0d36cbed2473bf8cae96ba1d04fa30be76e
VictorAlagwu <victoralagwu@gmail.com>
2015-11-15 15:48:17 +0100

    CheckOddEven.java

---
+++ CheckOddEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class data{
+	/*
+	Written by Victor Alagwu
+	 write a program called CheckOddEven which prints "Odd Number"if the int variable
+	  "number" is odd,or "Even number otherwise
+	 */
+	
+	public static void main( String[] args){
+		Scanner input=new Scanner(System.in);
+		int num;
+		System.out.print("Enter the num: ");
+		num=input.nextInt();
+		if ( num % 2 == 0){
+			System.out.println("Even Number");
+		}else 
+			System.out.println("Odd Number");
+	}
+}
